b3786d50c776a2f8425973becf231fbd2251db1b
fika4life <fistewie@gmail.com>
2015-03-16 15:18:53 +0100

    begining writing sql queries

--- src/Review.java
+++ src/Review.java
@@ -9,12 +9,25 @@ public class Review {
     private int rating;
     private String text;
     private int userid;
+    private int courseid;
+    private int teacherid;
 
-    public Review(Date time, int rating, String text, int userid) {
+    public Review(Date time, int rating, String text, int userid, int courseid, int teacherid) {
         this.time = time;
         this.rating = rating;
         this.text = text;
         this.userid = userid;
+        this.courseid = courseid;
+        this.teacherid = teacherid;
+    }
+
+    public Review(int rating, String text, int userid, int courseid, int teacherid) {
+
+        this.rating = rating;
+        this.text = text;
+        this.userid = userid;
+        this.courseid = courseid;
+        this.teacherid = teacherid;
     }
 
     public Date getTime() {
@@ -32,4 +45,13 @@ public class Review {
     public int getUserid(){
         return userid;
     }
+
+    public int getCourseid() {
+        return courseid;
+    }
+
+    public int getTeacherid() {
+        return teacherid;
+    }
+
 }
