aec0e6e0f44dd20a485d854276791ccf9ddef3d8
fika4life <fistewie@gmail.com>
2015-03-19 16:20:59 +0100

    removebookmark servlet

--- src/Servletts/AddAccount.java
+++ src/Servletts/AddAccount.java
@@ -1,10 +1,15 @@
 package Servletts;
 
+import DatabaseStuff.DatabaseHandler;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 /*
     In parameters:
@@ -19,6 +24,20 @@ public class AddAccount extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        Connection conn =  null;
+        PrintWriter writer = resp.getWriter();
+
+        try {
+            conn = DatabaseStuff.DbConnect.getConnection();
+            DatabaseStuff.DatabaseHandler db = new DatabaseHandler(conn);
+
+            db.addUser()
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
     }
 }
