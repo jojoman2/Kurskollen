73187d8f58c1c03c68a9f712177ba586b431121b
Fredrik <fredde_wallen@hotmail.com>
2015-03-19 14:13:25 +0100

    Created lots of servlet skins

---
+++ src/Servletts/Login.java
@@ -0,0 +1,18 @@
+package servletts;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Fredrik on 2015-03-19.
+ */
+public class Login extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //Check if the login was correct and if so send a loginsession id to the client
+    }
+}
---
+++ src/Servletts/SavedCourses.java
@@ -0,0 +1,18 @@
+package servletts;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Fredrik on 2015-03-19.
+ */
+public class SavedCourses extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
