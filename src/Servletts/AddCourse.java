fdbe23e479ad4d5b33fa5528171af2b1f45ffb33
fika4life <fistewie@gmail.com>
2015-03-26 15:49:16 +0100

    working on addcourse

--- src/Servletts/AddCourse.java
+++ src/Servletts/AddCourse.java
@@ -5,6 +5,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 /**
  * Created by Fredrik on 2015-03-19.
@@ -13,6 +15,33 @@ public class AddCourse extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        Connection conn = null;
+        try {
+            conn = DatabaseStuff.DbConnect.getConnection();
+            DatabaseStuff.DatabaseHandler db = new DatabaseStuff.DatabaseHandler(conn);
+
+            String creditsString = req.getParameter("credits");
+            float credits = -1;
+            if (creditsString != null){
+                credits  = Float.parseFloat("creditsString");
+            }
+
+            String schoolIdString = req.getParameter("schoolid");
+            schoolid = Integer.parseInt("schoolid");
+
+
+            String
+
+            Course course =  new Course(req.getParameter("coursecode"), req.getParameter("name"), req.getParameter("description"), credits, )
+            db.addCourse(course);
+
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
     }
 }
