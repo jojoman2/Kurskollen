aa5670cec5a7657435dab5754aded31303317a10
fika4life <fistewie@gmail.com>
2015-03-26 16:31:18 +0100

    changeusersettings finished not tested

--- src/Servletts/ChangeUserSettings.java
+++ src/Servletts/ChangeUserSettings.java
@@ -1,10 +1,14 @@
 package Servletts;
 
+import utils.ErrorChecker;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 /**
  * Created by Fredrik on 2015-03-19.
@@ -14,5 +18,21 @@ public class ChangeUserSettings extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-    }
+
+            Connection conn = null;
+            try {
+                conn = DatabaseStuff.DbConnect.getConnection();
+                DatabaseStuff.DatabaseHandler db = new DatabaseStuff.DatabaseHandler(conn);
+
+                db.changeUserDetails(req.getParameter("email"), req.getParameter("newName"), req.getParameter("newPassword"));
+                resp.setStatus(200);
+                
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+
 }
