654daa6d8e9a5763927fdd5a584a3929de4cfcbf
jojoman2 <fredde_wallen@hotmail.com>
2015-05-26 10:56:00 +0200

    Added schools constant

---
+++ src/utils/Constants.java
@@ -0,0 +1,54 @@
+package utils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Constants {
+
+    public static final Map<Integer,String> schools = new HashMap<Integer,String>(){{
+        put(1, "Blekinge tekniska högskola");
+        put(2, "Chalmers tekniska högskola");
+        put(3, "Dans och cirkushögskolan");
+        put(4, "Ersta Sköndal högskola");
+        put(5, "Försvarshögskolan Stockholm");
+        put(6, "Gymnastik- och idrottshögskolan");
+        put(7, "Göteborgs universitet");
+        put(8, "Handelshögskolan i Stockholm");
+        put(9, "Högskolan Dalarna");
+        put(10, "Högskolan i Borås");
+        put(11, "Högskolan i Gävle");
+        put(12, "Högskolan i Halmstad");
+        put(13, "Högskolan i Jönköping");
+        put(14, "Högskolan i Skövde");
+        put(15, "Högskolan Kristianstad");
+        put(16, "Högskolan Väst");
+        put(17, "Johannelunds teologiska högskola");
+        put(18, "Karlstads universitet");
+        put(19, "Karolinska institutet");
+        put(20, "Konstfack");
+        put(21, "Kungl. Musikhögskolan i Stockholm");
+        put(22, "Kungl. Tekniska högskolan");
+        put(23, "Linköpings universitet");
+        put(24, "Linnéuniversitetet (Kalmar Växjö)");
+        put(25, "Luleå tekniska universitet");
+        put(26, "Lunds universitet");
+        put(27, "Malmö högskola");
+        put(28, "Mittuniversitetet");
+        put(29, "Myndigheten för samhällsskydd och beredskap");
+        put(30, "Mälardalens högskola");
+        put(31, "Röda korsets högskola");
+        put(32, "SLU); Sveriges lantbruksuniversitet");
+        put(33, "Sophiahemmet Högskola");
+        put(34, "Stockholms dramatiska högskola");
+        put(35, "Stockholms Musikpedagogiska Institut");
+        put(36, "Stockholms universitet");
+        put(37, "Södertörns högskola");
+        put(38, "Teologiska Högskolan); Stockholm");
+        put(39, "UHR");
+        put(40, "Umeå universitet");
+        put(41, "Uppsala universitet");
+        put(42, "Örebro Teologiska Högskola");
+        put(43, "Örebro universitet");
+    }};
+
+}
